7fdc2075d8ac54cb3e6612a750d8c0f363b1ecd2
Robert Waffen <rw@betadots.de>
2026-03-05 12:12:28 +0100

    fix: remove debug logging for patching results in patch_batch plan

--- plans/patch_batch.pp
+++ plans/patch_batch.pp
@@ -88,14 +88,7 @@ plan os_patching::patch_batch (
   $no_patches      = $patching_result.ok_set.filter_set |$item| { $item.value['packages_updated'].empty }.names
   $with_patches    = $patching_result.ok_set.filter_set |$item| { ! $item.value['packages_updated'].empty }.names
   $reboot_required = $patching_result.ok_set.filter_set |$item| { $item.value['reboot_required'] == true }.names
-
-  if $debug {
-    out::message(type($no_patches))
-    out::message(type($with_patches))
-    out::message(type($reboot_required))
-    out::message(type($health_check_failed))
-  }
-  $targets = get_targets($batch).map |$t| { $t.name }.sort
+  $targets         = get_targets($batch).map |$t| { $t.name }.sort
 
   $output = {
     failed              => $patching_result.error_set.names,
